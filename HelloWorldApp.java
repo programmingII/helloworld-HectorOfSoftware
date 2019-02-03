86c64d1196435094ece556d70eafcc86902d718c
169840 Hector Sanchez <46726559+169840HectorSanchez@users.noreply.github.com>
2019-02-03 13:20:26 -0700

    Add files via upload
    Primer Codigo en Java obtenido en https://docs.oracle.com/javase/tutorial/getStarted/cupojava/win32.html

---
+++ HelloWorldApp.java
@@ -0,0 +1,10 @@
+/**
+*
+* The HelloWorldApp Class implements an application that
+*simply prints "Hello World!" to standard output.
+*/
+class HelloWorldApp{
+	public static void main(String[] args){
+		System.out.println("Hello World!"); //Display the string
+}
+}
